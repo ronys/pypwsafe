a84e2876cb187af4d44e9045ef0d80396ecd149b
Paulson McIntyre (GpMidi) <paul@gpmidi.net>
2011-08-17 21:05:26 -0400

    - Completed models - Completed load script

--- src/psafefe/psafe/models.py
+++ src/psafefe/psafe/models.py
@@ -17,15 +17,314 @@
 #===============================================================================
 from django.db import models
 from uuid import uuid4
+from django.contrib.auth.models import User, Group
+from psafefe.psafe.validators import *
+from os.path import join
 
-# Create your models here.
-class PasswordSafe(models.Model):
-    uuid = models.CharField(
+class PasswordSafeRepo(models.Model):
+    """ A place where psafes can be stored """
+    class Meta:
+        ordering = [
+                    'name',
+                    ]
+        abstract = True
+        verbose_name = "Password Safe Repo"
+        verbose_name_plural = "Password Safe Repos"
+        
+    name = models.CharField(
                             null = False,
-                            default = str(uuid4()),
+                            blank = False,
+                            length = 255,
+                            verbose_name = "Name",
+                            help_text = "A human readable name for the password safe repository",
+                            )
+    path = models.CharField(
+                            null = False,
+                            blank = False,
+                            length = 1024 * 1024,
+                            verbose_name = "Server Location",
+                            help_text = "The location on the server of the password safes",
+                            validators = [
+                                        validate_r_ok,
+                                        ],
+                            )
+    adminGroups = models.ManyToManyField(
+                                           Group,
+                                           verbose_name = "Admin Groups",
+                                           help_text = "Groups that have administrative access to this repo",
+                                           )
+    readAllowGroups = models.ManyToManyField(
+                                               Group,
+                                               verbose_name = "Read-Allow Groups",
+                                               help_text = "Groups that have read access to this repo",
+                                               )
+    writeAllowGroups = models.ManyToManyField(
+                                               Group,
+                                               verbose_name = "Write-Allow Groups",
+                                               help_text = "Groups that have write access to this repo",
+                                               )
+    # These are applied before the allows
+    readDenyGroups = models.ManyToManyField(
+                                               Group,
+                                               verbose_name = "Read-Deny Groups",
+                                               help_text = "Groups that do not have read access to this repo. This overrides the read-allow groups list. ",
+                                               )
+    writeDenyGroups = models.ManyToManyField(
+                                               Group,
+                                               verbose_name = "Write-Deny Groups",
+                                               help_text = "Groups that do not have write access to this repo. This overrides the write-allow groups list. ",
+                                               )
+    
+    # Random ideas: 
+    # Include options for storing all safes in a GIT repo
+    # Add per-user permissions too
+    # Add user-created groups or something to that effect
+    
+class PasswordSafe(models.Model):
+    """ Keep a record of all psafes that we should track
+    Do NOT store any confidential info from the safe. 
+    """
+    class Meta:
+        ordering = [
+                    'repo',
+                    'filename',
+                    'uuid',
+                    ]
+        verbose_name = "Password Safe"
+        verbose_name_plural = "Password Safes"
+        unique_together = (
+                           # Can't do this because filename is too long
+                           # TODO: Add a filename_md5 or something 
+                           # ('filename','repo'),
+                           )
+        
+    uuid = models.CharField(
+                            # can't use as PK as two psafes may have the same uuid
+                            # primary_key = True,
+                            null = False,
+                            # Make it a callable otherwise all will default to the same (at least within one instance)
+                            default = lambda: str(uuid4()),
                             length = 36,
                             verbose_name = "UUID",
                             help_text = "Password Safe GUID",
                             editable = False,
                             )
+    filename = models.CharField(
+                                # The system should note this safe as "missing" if it can't be found atm. 
+                                null = True,
+                                length = 1024 * 1024,
+                                verbose_name = "Password Safe Path",
+                                help_text = "The full path to the password safe from the worker's perspective",
+                                )
+    repo = models.ForeignKey(
+                             PasswordSafeRepo,
+                             verbose_name = "Repository",
+                             help_text = "The password safe repository that this safe resides in",
+                             )
     
+    owner = models.ForeignKey(
+                              User,
+                              # If null it's a normal psafe, if set, it's a personal psafe
+                              null = True,
+                              verbose_name = "Owner",
+                              help_text = "The owning user of the password safe",
+                              ) 
+    
+    def psafePath(self):
+        """ Returns the full path on the server to the psafe file """
+        return join(self.repo.path, self.filename)
+    
+# Memory resident tables
+class MemPSafe(models.Model):
+    """ Represent a cache'd psafe """
+    safe = models.ForeignKey(
+                             PasswordSafe,
+                             null = False,
+                             verbose_name = "Password Safe File",
+                             help_text = "Refrence to the psafe file",
+                             editable = False,
+                             )
+    uuid = models.CharField(
+                            # can't use as PK as two psafes may have the same uuid
+                            # primary_key = True,
+                            null = False,
+                            # Make it a callable otherwise all will default to the same (at least within one instance)
+                            default = lambda: str(uuid4()),
+                            length = 36,
+                            verbose_name = "UUID",
+                            help_text = "Password Safe GUID",
+                            editable = False,
+                            )
+    dbName = models.CharField(
+                              null = True,
+                              default = None,
+                              blank = True,
+                              length = 1024 * 1024,
+                              verbose_name = "Database Name",
+                              )
+    dbDescription = models.TextField(
+                                     null = True,
+                                     blank = True,
+                                     default = None,
+                                     length = 1024 * 1024 * 1024,
+                                     verbose_name = "Database Description",
+                                     )
+    dbPassword = models.CharField(
+                                  null = False,
+                                  blank = True,
+                                  default = "bogus12345",
+                                  length = 1024 * 1024,
+                                  verbose_name = "Database Password",
+                                  )
+    dbTimeStampOfLastSafe = models.DateTimeField(
+                                                 null = True,
+                                                 verbose_name = "Last Save",
+                                                 help_text = "Date/Time of last Password Safe save",
+                                                 )
+    dbLastSaveApp = models.CharField(
+                                     null = True,
+                                     verbose_name = "Last Save App",
+                                     )
+    dbLastSaveHost = models.CharField(
+                                     null = True,
+                                     verbose_name = "Last Save Host",
+                                     )
+    dbLastSaveUser = models.CharField(
+                                     null = True,
+                                     verbose_name = "Last Save User",
+                                     )
+    # Cache params
+    fileLastModified = models.DateTimeField(
+                                            null = False,
+                                            verbose_name = "File Last Modified",
+                                            editable = False,
+                                            )
+    fileLastSize = models.IntegerField(
+                                      null = False,
+                                      verbose_name = "File Last Size",
+                                      editable = False,
+                                      )
+    # TODO: Add in safe HMAC validation checks too
+    
+class MemPsafeEntry(models.Model):
+    """ Represent a cached password safe entry """
+    class Meta:        
+        unique_together = (
+                           ('safe', 'uuid'),
+                           )
+    safe = models.ForeignKey(
+                             MemPSafe,
+                             null = False,
+                             verbose_name = "Password Safe",
+                             )
+    uuid = models.CharField(
+                            # can't use as PK as two psafes may have the same uuid
+                            # primary_key = True,
+                            null = False,
+                            # Make it a callable otherwise all will default to the same (at least within one instance)
+                            default = lambda: str(uuid4()),
+                            length = 36,
+                            verbose_name = "UUID",
+                            help_text = "Entry GUID",
+                            editable = False,
+                            )
+    group = models.CharField(
+                             null = True,
+                             default = None,
+                             length = 4096,
+                             verbose_name = "Group",
+                             help_text = "Dot separated group listing for the entry",
+                             )
+    title = models.CharField(
+                             null = True,
+                             default = None,
+                             length = 4096,
+                             verbose_name = "Title",
+                             )
+    username = models.CharField(
+                             null = True,
+                             default = None,
+                             length = 4096,
+                             verbose_name = "Username",
+                             )
+    notes = models.TextField(
+                             null = True,
+                             default = None,
+                             length = 1024 * 1024,
+                             verbose_name = "Notes",
+                             )
+    password = models.CharField(
+                             null = True,
+                             default = None,
+                             length = 4096,
+                             verbose_name = "Password",
+                             )
+    creationTime = models.DateTimeField(
+                                        null = True,
+                                        default = None,
+                                        verbose_name = "Creation Time",
+                                        )
+    passwordModTime = models.DateTimeField(
+                                           null = True,
+                                           default = None,
+                                           verbose_name = "Password Last Modification Time",
+                                           )
+    accessTime = models.DateTimeField(
+                                      null = True,
+                                      default = None,
+                                      verbose_name = "Last Access Time",
+                                      )
+    passwordExpiryTime = models.DateTimeField(
+                                              null = True,
+                                              verbose_name = "Password Expiry Time",
+                                              )
+    modTime = models.DateTimeField(
+                                   null = True,
+                                   verbose_name = "Last Modification Time",
+                                   )
+    # Don't use a URL field - We don't want to risk any validation
+    # Plus psafe doesn't guarantee it's a URL
+    url = models.CharField(
+                           null = True,
+                           default = None,
+                           length = 4096,
+                           verbose_name = "URL",
+                           )
+    autotype = models.CharField(
+                           null = True,
+                           default = None,
+                           length = 4096,
+                           verbose_name = "Autotype String",
+                           )
+    runCommand = models.CharField(
+                           null = True,
+                           default = None,
+                           length = 4096,
+                           verbose_name = "Run Command",
+                           )
+    # Don't use an email field as psafe doesn't make any guarantees about the value
+    email = models.CharField(
+                           null = True,
+                           default = None,
+                           length = 4096,
+                           verbose_name = "Email",
+                           )
+
+class MemPasswordEntryHistory(models.Model):
+    """ Old passwords for the given entry """
+    entry = models.ForeignKey(
+                              MemPsafeEntry,
+                              null = False,
+                              verbose_name = "Entry",
+                              )
+    password = models.CharField(
+                             null = True,
+                             default = None,
+                             length = 4096,
+                             verbose_name = "Old Password",
+                             )
+    creationTime = models.DateTimeField(
+                                        null = True,
+                                        default = None,
+                                        verbose_name = "Creation Time",
+                                        )
--- src/psafefe/psafe/tasks/load.py
+++ src/psafefe/psafe/tasks/load.py
@@ -21,7 +21,109 @@ Created on Aug 16, 2011
 @author: gpmidi
 '''
 from celery.task import task #@UnresolvedImport
+from psafefe.psafe.models import *
+from psafefe.psafe.errors import *
+from pypwsafe import PWSafe3
+import stat
 
-@task
-def Test():
-    return "Hai"
+@task()
+def loadSafe(psafe_pk, password, force = False):
+    """ Cache  password safe. Returns True if the cache was updated. False otherwise. """
+    try:
+        psafe = PasswordSafe.objects.get(pk = psafe_pk)
+    except PasswordSafe.DoesNotExist:
+        raise PasswordSafeDoesntExist, "Password safe object %r doesn't exist" % psafe_pk
+    if not os.access(psafe.psafePath(), os.R_OK):
+        raise NoAccessToPasswordSafe, "Can't read psafe file %r" % psafe.psafePath()
+    try:
+        memPSafe = MemPSafe.objects.get(safe = psafe)
+    except MemPSafe.DoesNotExist:
+        memPSafe = MemPSafe(
+                            safe = psafe,
+                            )
+    # Check if we need to
+    if not force and os.stat(psafe.psafePath())[stat.ST_MTIME] == memPSafe.fileLastModified and memPSafe.fileLastSize == os.stat(psafe.psafePath())[stat.ST_SIZE]:
+        return False
+    
+    # Save first, just in case
+    memPSafe.fileLastModified = os.stat(psafe.psafePath())[stat.ST_MTIME]
+    memPSafe.fileLastSize = os.stat(psafe.psafePath())[stat.ST_SIZE]
+    
+    # Let standard psafe errors travel on up    
+    pypwsafe = PWSafe3(
+                     filename = psafe.psafePath(),
+                     password = password,
+                     mode = "R",
+                     )
+    # Update/set attributes
+    memPSafe.uuid = pypwsafe.getUUID()
+    memPSafe.dbName = psafe.getDbName()
+    memPSafe.dbDescription = psafe.getDbDesc()
+    memPSafe.dbPassword = password
+    memPSafe.dbTimeStampOfLastSafe = psafe.getTimeStampOfLastSafe()
+    memPSafe.dbLastSaveApp = psafe.getLastSaveApp()
+    memPSafe.dbLastSaveHost = psafe.getLastSaveHost()
+    memPSafe.dbLastSaveUser = psafe.getLastSaveUser()
+    
+    memPSafe.save()
+    
+    # All entries in db. Remove from list after updating.  
+    remaining = {}
+    for i in MemPsafeEntry.objects.filter(safe = pypwsafe):
+        if i.uuid in remaining: 
+            raise DuplicateUUIDError, "Entry %r has the same UUID as %r" % (i, remaining[i.uuid])
+        else:
+            remaining[i.uuid] = i
+        
+    updated = {}
+    
+    for entry in pypwsafe.getEntries():
+        # Find the entry to create it (by uuid)
+        if entry.getUUID() in remaining:
+            memEntry = remaining[entry.getUUID()]
+            del remaining[entry.getUUID()]
+            updated[entry.getUUID()] = memEntry
+        else:
+            memEntry = MemPsafeEntry(
+                                   safe = memPSafe,
+                                   )
+        # Update the entry
+        memEntry.group = '.'.join(entry.getGroup())
+        memEntry.title = entry.getTitle()
+        memEntry.username = entry.getUsername()
+        memEntry.notes = entry.getNote()
+        memEntry.password = entry.getPassword()
+        memEntry.creationTime = entry.getCreated()
+        memEntry.passwordModTime = entry.getPasswordModified()
+        memEntry.accessTime = entry.getLastAccess()
+        memEntry.passwordExpiryTime = entry.getExpires()
+        memEntry.modTime = entry.getEntryModified()
+        memEntry.url = entry.getURL()
+        memEntry.autotype = entry.getAutoType()
+        memEntry.runCommand = entry.getRunCommand()
+        memEntry.email = entry.getEmail()
+        
+        memEntry.save()
+        
+        org = {}
+        for i in MemPasswordEntryHistory.objects.filter(entry = memEntry):
+            org[repr(i.creationTime) + i.password] = i
+        found = [] 
+        for old in memEntry.getHistory():
+            t = repr(repr(old['saved']) + old['password'])
+            if t in org:
+                memOld = org[t]
+                del org[t]
+                found.append(memOld)
+            else:
+                memOld = MemPasswordEntryHistory(entry = memEntry, password = old['password'], creationTime = old['saved'])
+                memOld.save()
+        # Remove all other old password entries
+        for removedEntry in org.values():
+            removedEntry.delete()
+    # Remove all other entries
+    for removedEntry in remaining.values():
+        removedEntry.delete()
+    
+    return True
+        
